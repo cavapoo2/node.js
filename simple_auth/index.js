2959fe76c4d09d0eb3de76a77d562f0aa54eae9a
Poodle <forpetessake@protonmail.com>
2018-07-12 11:22:48 +0100

    Create index.js

---
+++ simple_auth/index.js
@@ -0,0 +1,54 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const redis = require("redis");
+const client = redis.createClient();
+app.use("/js", express.static(__dirname + '/js'));
+app.get('/authenticate/:username', function(request, response){
+	let publicKey = Math.random();
+	let username = request.params.username; // This is always "jack"
+	
+	// ... get jack's data from redis
+	client.hgetall(username, (err, data) => {
+		if(err || !data) {
+			return response.end("no data");
+		}
+		//	Jack's password is always "beanstalk"
+		let challenge = crypto.createHash('sha256').update(publicKey + data.password).digest('hex');
+
+		//	Store challenge for later match
+		client.set(challenge, username);
+		
+		//response.end(challenge);
+		response.end(publicKey + "");
+	});
+});
+
+app.get('/login/:response', (request, response) => {
+	let hash = request.params.response;
+  	client.exists(hash, function(err, exists) {
+  		if(err || !exists) {
+  			return response.end("failed");
+  		}
+  	});
+  
+  	client.del(hash, () => response.end("OK"));
+});
+
+app.get('/', (request, response) => {
+    fs.readFile('./index.html', (err, data) => {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(data);
+    });
+});
+
+app.get('/256', (request, response) => {
+	console.log('256');
+	fs.readFile('js/sha256.js', (err, data) => {
+        response.end(data);
+    });
+});
+
+app.listen(8080);
+
