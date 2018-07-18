8d3c391adee6c72e533144a40a33e997bb7983c1
Poodle <forpetessake@protonmail.com>
2018-07-18 09:52:06 +0100

    Update script.js

--- pm2_process/script.js
+++ pm2_process/script.js
@@ -2,6 +2,8 @@
 //./node_modules/pm2/bin/pm2 start script.js
 //./node_modules/pm2/bin/pm2 list
 //./node_modules/pm2/bin/pm2 info script
+//logs are in .pm2 in home directory
+//clear logs by doing flush
 'use strict';
 // script.js
 let count = 1;
