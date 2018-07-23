46af07be069107f89cf6bf56f15101277fa1de7f
Poodle <forpetessake@protonmail.com>
2018-07-23 16:01:00 +0100

    Update topic.js

--- simple_rabbit_mq/topic.js
+++ simple_rabbit_mq/topic.js
@@ -2,24 +2,56 @@ const amqp = require('amqp');
 const consumer = amqp.createConnection({ host: 'localhost', port: 5672 });
 
 consumer.on('ready', () => {
-	let exchange = consumer.exchange('node-direct-exchange', {type: "direct"});
-	consumer.queue('node-topic-queue', q => {
+	
+	// When all 3 queues are ready, publish.
+	let cnt = 3;
+	let queueReady = () => {
+		if(--cnt > 0) {
+			return;
+		}
+		exchange.publish("animals.dogs.poodles", "Poodle!");
+		exchange.publish("animals.dogs.dachshund", "Dachshund!");
+		exchange.publish("animals.cats.shorthaired", "Shorthaired Cat!");
+		exchange.publish("animals.dogs.shorthaired", "Shorthaired Dog!");
+		exchange.publish("animals.misc", "Misc!");
+	};
+	
+	let exchange = consumer.exchange('topical', {type: "topic"});
+	
+	consumer.queue('queue-1', q => {
 
-		q.bind(exchange, 'room1');
+		q.bind(exchange, 'animals.*.shorthaired');
 		q.subscribe(message => {
-			console.log("room1 received: " + message.data.toString('utf8'));
+			console.log('animals.*.shorthaired -> ' + message.data.toString('utf8'));
 		});
 		
-		q.bind(exchange, 'room2');
+		queueReady();
+	});
+
+	consumer.queue('queue-2', q => {
+		q.bind(exchange, '#');
 		q.subscribe(message => {
-			console.log("room2 received: " + message.data.toString('utf8'));
+			console.log('# -> ' + message.data.toString('utf8'));
 		});
 		
-		exchange.publish("room1", "Hello Room1!");
-		exchange.publish("room2", "Hello Room2!");
+		queueReady();
+	});
+
+	consumer.queue('queue-3', q => {
+		q.bind(exchange, '*.cats.*');
+		q.subscribe(message => {
+			console.log('*.cats.* -> ' + message.data.toString('utf8'));
+		});
 		
-		//	Nobody should hear this
-		//
-		exchange.publish("room3", "Hello Room3!");
+		queueReady();
 	});
 });
+
+//	# -> Poodle!
+//	animals.*.shorthaired -> Shorthaired Cat!
+//	*.cats.* -> Shorthaired Cat!
+//	# -> Dachshund!
+//	# -> Shorthaired Cat!
+//	animals.*.shorthaired -> Shorthaired Dog!
+//	# -> Shorthaired Dog!
+//	# -> Misc!
